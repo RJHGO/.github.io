f5c760ceab60bf174ec400d695fc4ccb9afdc773
RJHGO <leftantenna@msn.com>
2020-03-11 15:56:14 -0700

    update portfolio

---
+++ JS/Portfolio.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+    $("a").on("click", function(event) {
+        if (this.Link !== "") {
+            event.preventDefault();
+            var Link = this.Link;
+            $("html, body").animate({
+                scrollTop: $(Link).offset().top
+            }, 2000, function(){
+                window.location.Link = Link;            
+            });
+        }
+    });   
+});
+
+function HiddenForm(){
+    document.getElementById("Contact_Form").style.display = "block";
+}
+
+function CloseForm(){
+    document.getElementById("Contact_Form").style.display = "none";
+    this.Close();
+}
